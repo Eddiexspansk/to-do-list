62ad49ad302e57ef4a229d43a40bddfe6fe3edb9
Eddiexspansk <spansk_cl@hotmail.com>
2023-03-28 16:33:58 +0100

    git push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css'
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 import ListaDeTareas from './componentes/ListaDeTareas';
 
 
 function App() {
   return (
     <div className="aplicacion-tareas">
-      <div className='frerecodecamp-logo-contenedor'>
-        <img
-          src={freeCodeCampLogo}
-          className='freecodecamp-logo'
-          />
-      </div>
-
       <div className='tareas-lista-principal'>
         <h1>Mis Tareas</h1>
         <ListaDeTareas />
